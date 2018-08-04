2ddccfb1576c2f754ab7e0785b33292329b204bc
2303-66\Students <dk_thanitpon@hotmail.com>
2018-08-04 17:14:40 +0700

    ddd

---
+++ resources/views/test.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel</title>
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+        <h1> 
+@if($mods)
+    <ul>
+    @foreach($mods as $item)
+        <li>{{ $item->name.' : '.$item->surname }}</li>
+    @endforeach
+    </ul>
+@endif
+
+
+</h1>
+    </body>
+</html>
+
